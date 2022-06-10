617b0b54acaded6162773b404ddffd2e5b88ee03
kabutoshinki <kabutoshinki@gmail.com>
2022-06-10 22:13:30 +0700

    add recursion to see GREEN 4EVER AGAIN

--- src/com/kabutoshinki/mathutil/core/MathUtil.java
+++ src/com/kabutoshinki/mathutil/core/MathUtil.java
@@ -14,19 +14,29 @@ public class MathUtil {
   // 0!=1!=1
   //21! kieu long
   //< 0! ko tính dc, ko có giai thua cho so am  
-  public static long getFactorial(int n){
-      if(n<0 || n>20){
+    
+    public static long getFactorial(int n){
+        if(n<0 || n>20){
           throw new IllegalArgumentException("n much be between 0 ... 20");
       }
       if(n==0 || n==1 ){
           return 1;
       }
-      long product =1;
-      for(int i=2;i<=n;i++){
-          product*=i;
-      }
-      return product;
-  } 
+       return n * getFactorial(n-1);
+    }
+//  public static long getFactorial(int n){
+//      if(n<0 || n>20){
+//          throw new IllegalArgumentException("n much be between 0 ... 20");
+//      }
+//      if(n==0 || n==1 ){
+//          return 1;
+//      }
+//      long product =1;
+//      for(int i=2;i<=n;i++){
+//          product*=i;
+//      }
+//      return product;
+//  } 
     
     
 }
